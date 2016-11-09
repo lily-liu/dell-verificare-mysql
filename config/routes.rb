0c2c5e1a93d41e9fa2bdfbbfb52084c8adc7448f
ftumj259 <2013470259@ftumj.ac.id>
2016-11-09 11:42:13 +0700

    This is ignoring 66

--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  post "users/login", to: "users#login"
-  resources :users
-end
