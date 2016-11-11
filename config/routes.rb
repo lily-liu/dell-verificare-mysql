8bfedcc1b4bf7b91f32e2fe295bb35e85c2bedb3
wisely <wisely.dennis@student.sgu.ac.id>
2016-11-11 14:44:47 +0700

    test deploy eb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/", to: "users#login"
-  post "users/login", to: "users#login"
+  # post "users/login", to: "users#login"
   resources :users
 end
